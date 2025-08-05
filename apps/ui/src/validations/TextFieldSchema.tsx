59c8d1b3d0f173d92631d832a550bea5c30a8999
Thejana Hirumal <thejanahirumal5@gmail.com>
2025-08-05 10:40:38 +0530

    chore:fix-alignment-issues

--- apps/ui/src/validations/TextFieldSchema.tsx
+++ apps/ui/src/validations/TextFieldSchema.tsx
@@ -7,10 +7,5 @@ export const emailField = yup
 
 export const passwordField = yup
   .string()
-    .required("New password is required")
-    .min(8, "Password must be at least 8 characters")
-    .max(12, "Password must be at most 12 characters")
-    .matches(/[A-Z]/, "Must contain at least one uppercase letter")
-    .matches(/[a-z]/, "Must contain at least one lowercase letter")
-    .matches(/\d/, "Must contain at least one number")
-    .matches(/[@$!%*?&#^(){}[\]<>+=~|\\/:;"',.-]/, " At least one special character");
+    .required("password is required")
+    
